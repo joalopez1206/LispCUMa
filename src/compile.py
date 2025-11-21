a331a95699a560103d01e73cceff60c678a6f86a
joalopez1206 <shakedrizle@gmail.com>
2025-11-21 18:21:57 -0300

    feat: enhance compile_expr to support Let expressions and improve environment handling

--- src/compile.py
+++ src/compile.py
@@ -1,15 +1,36 @@
 from src.expr import Expr
-from src.lang import Num, Add1
-from src.asm import Reg, Const, IMov, IAdd1
+from src.lang import Num, Add1, Let, Id
+from src.asm import Reg, Const, IMov, IAdd1, IRet, RegOffset
 from src.asm import instruction, pprint_instrs
+from dataclasses import dataclass
 
 
-def compile_expr(expr: Expr, env: list) -> list[instruction]:
+type compEnv =  list[tuple[str, int]]
+
+def add(env: compEnv, name: str) -> compEnv:
+    i = len(env)
+    return [(name, i)] + env
+
+def lookup(env: compEnv, s:str):
+    match env:
+        case [(a, i), *xs]:
+            return i if a == s else lookup(xs, s)
+        case _:
+            raise ValueError()
+
+def compile_expr(expr: Expr, env: compEnv) -> list[instruction]:
     match expr:
+        case Id(x):
+            offset = lookup(env, x)
+            return [IMov(Reg.RAX, RegOffset(Reg.RSP, offset))]
         case Num(n):
             return [IMov(Reg.RAX, Const(n))]
         case Add1(n):
             return compile_expr(n, env) + [IAdd1(Reg.RAX)]
+        case Let(var, nexpr, body):
+            nenv = add(env, var.name)
+            instrs = compile_expr(nexpr, env)
+            return instrs + compile_expr(body, nenv)
         case _:
             raise NotImplementedError(f"Compilation not implemented for {expr}")
 
@@ -23,4 +44,4 @@ def compile(expr: Expr) -> str:
         "global our_code_starts_here\n"
         "our_code_starts_here:"
     )
-    return prelude + "\n" + pprint_instrs(instrs)
+    return prelude + "\n" + pprint_instrs(instrs + [IRet()])
