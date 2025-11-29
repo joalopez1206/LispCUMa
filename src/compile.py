f457cb2198f48fbac222ffb3aa2516dd8ddd3043
joalopez1206 <shakedrizle@gmail.com>
2025-11-29 17:32:48 -0300

    format ruff

--- src/compile.py
+++ src/compile.py
@@ -4,31 +4,35 @@ from src.asm import Reg, Const, IMov, IAdd1, IRet, RegOffset, IAdd
 from src.asm import instruction, pprint_instrs
 
 
-type compEnv =  list[tuple[str, int]]
-
+type compEnv = list[tuple[str, int]]
 
 
 class Gensym:
     def __init__(self, s: str):
         self.s = s
         self.i = 0
-    
+
     def gen(self) -> str:
-        return f"{self.s}{self.i}"
+        s = f"{self.s}{self.i}"
+        self.i += 1
+        return s
 
 
 gensym = Gensym("x")
+
 
 def add(env: compEnv, name: str) -> tuple[compEnv, int]:
     i = 1 + len(env)
     return [(name, i)] + env, i
 
-def lookup(env: compEnv, s:str):
+
+def lookup(env: compEnv, s: str):
     match env:
         case [(a, i), *xs]:
             return i if a == s else lookup(xs, s)
         case _:
             raise ValueError()
+
 
 def compile_expr(expr: Expr, env: compEnv) -> list[instruction]:
     match expr:
@@ -39,35 +43,39 @@ def compile_expr(expr: Expr, env: compEnv) -> list[instruction]:
             return [IMov(Reg.RAX, Const(n))]
         case Add1(n):
             return compile_expr(n, env) + [IAdd1(Reg.RAX)]
-        case Add(l,r):
+        case Add(l, r):
             lsym = gensym.gen()
             rsym = gensym.gen()
-            en1, loffset  = add(env,lsym)
+            en1, loffset = add(env, lsym)
             en2, roffset = add(en1, rsym)
-
 
             left_instr = compile_expr(l, en2)
             lstack = [IMov(RegOffset(Reg.RSP, loffset), Reg.RAX)]
-            right_instr = compile_expr(r,en2)
+            right_instr = compile_expr(r, en2)
             rstack = [IMov(RegOffset(Reg.RSP, roffset), Reg.RAX)]
             leave_res_in_rax = [IMov(Reg(Reg.RAX), RegOffset(Reg.RSP, loffset))]
 
-            return left_instr + lstack + right_instr + rstack + leave_res_in_rax + [IAdd(Reg.RAX, RegOffset(Reg.RSP, roffset))]
+            return (
+                left_instr
+                + lstack
+                + right_instr
+                + rstack
+                + leave_res_in_rax
+                + [IAdd(Reg.RAX, RegOffset(Reg.RSP, roffset))]
+            )
         case Let(var, nexpr, body):
             nenv, slot = add(env, var.name)
             instrs = compile_expr(nexpr, env)
-            return instrs + [IMov(RegOffset(Reg.RSP, slot), Reg.RAX)] + compile_expr(body, nenv)
+            return (
+                instrs
+                + [IMov(RegOffset(Reg.RSP, slot), Reg.RAX)]
+                + compile_expr(body, nenv)
+            )
         case _:
             raise NotImplementedError(f"Compilation not implemented for {expr}")
 
 
-
-
 def compile(expr: Expr) -> str:
     instrs = compile_expr(expr, [])
-    prelude = (
-        "section .text\n"
-        "global our_code_starts_here\n"
-        "our_code_starts_here:"
-    )
+    prelude = "section .text\nglobal our_code_starts_here\nour_code_starts_here:"
     return prelude + "\n" + pprint_instrs(instrs + [IRet()])
